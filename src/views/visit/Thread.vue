d57a49de47713ef6e1fe4bb1e48db2d8ad700d64
User大帅 <git config --global user.email you@example.com>
2020-06-10 17:37:50 +0800

    完善访客访程功能

--- src/views/visit/Thread.vue
+++ src/views/visit/Thread.vue
@@ -3,7 +3,6 @@
     <title-bar>
       <span slot="title">当前访程</span>
       <span slot="control">
-        <el-button type="primary" size="small" @click="handleAdd">新增</el-button>
       </span>
     </title-bar>
     <search-bar>
@@ -15,7 +14,7 @@
       <el-table
         :data="threadData"
         style="width: 100%"
-        stripe
+        :row-style="tableRowStyle"
         empty-text="暂无数据">
         <el-table-column
           prop="name"
@@ -50,6 +49,19 @@
           label="截至时间">
         </el-table-column>
         <el-table-column
+          prop="surplus"
+          align="center"
+          :formatter="formatSurplus"
+          show-overflow-tooltip
+          label="剩余时间">
+        </el-table-column>
+        <el-table-column
+          prop="status"
+          align="center"
+          show-overflow-tooltip
+          label="访问状态">
+        </el-table-column>
+        <el-table-column
           prop="des"
           align="center"
           show-overflow-tooltip
@@ -67,11 +79,13 @@
         <el-table-column
           align="center"
           label="操作">
-          <template>
+          <template slot-scope="scope">
             <el-button style="color:#F56C6C;" type="text"
-              @click="deleteInfo">删除</el-button>
+              @click="deleteInfo(scope.row.id)">结束访程</el-button>
             <el-button style="color:#409EFF;" type="text"
-              @click="editThread">编辑</el-button>
+              @click="editThread(scope.row.id)">修改时间</el-button>
+            <el-button style="color:#409EFF;" type="text"
+              @click="editLimit(scope.row.id)">修改权限</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -87,44 +101,90 @@
       :close-on-click-modal="$store.state.closeOnClickModal">
       <img :src="previewImg" alt="图片预览" class="preImg">
     </el-dialog>
-    <!-- 创建访程表单 -->
+    <!-- 修改访程表单 -->
     <el-dialog :visible.sync="eTDialog" width="30%"
       custom-class="limit-dialog"
-      title="创建访程" top="50vh"
+      title="修改结束时间" top="50vh"
       :show-close="false"
       :close-on-press-escape="$store.state.closeOnPresEscape"
       :close-on-click-modal="$store.state.closeOnClickModal">
-      <el-form style="width:90%" :rules="threadFormRule" :model="threadForm" ref="threadForm"
-        label-width="7vw" label-position="right">
-        <el-form-item label="起始时间" prop="startTime">
-          <el-date-picker
-            v-model="threadForm.startTime"
-            type="datetime"
-            value-format="timestamp"
-            placeholder="选择日期时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="结束时间" prop="endTime">
-          <el-date-picker
-            v-model="threadForm.endTime"
-            type="datetime"
-            value-format="timestamp"
-            placeholder="选择日期时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="备注" prop="des">
-          <el-input v-model="threadForm.des"></el-input>
-        </el-form-item>
-        <el-form-item label="授权设备" prop="deviceIds">
-          <el-checkbox-group v-model="threadForm.deviceIds">
-            <el-checkbox v-for="(item,index) in devices" :key="index"
-              :label="item.id">{{item.name}}</el-checkbox>
-          </el-checkbox-group>
-        </el-form-item>
-      </el-form>
+      <el-date-picker
+        v-model="endTime"
+        type="datetime"
+        value-format="timestamp"
+        placeholder="选择日期时间">
+      </el-date-picker>
       <span slot="footer" class="dialog-footer">
         <el-button @click="cancelThread">取 消</el-button>
         <el-button type="primary" @click="submitThread">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 修改访客权限 -->
+    <el-dialog :visible.sync="eLDialog" width="50%"
+      custom-class="limit-dialog"
+      title="修改访客权限" top="50vh"
+      :show-close="false"
+      :close-on-press-escape="$store.state.closeOnPresEscape"
+      :close-on-click-modal="$store.state.closeOnClickModal">
+      <el-scrollbar style="max-height:50vh">
+        <el-table :data="bindDevice" 
+          style="width:100%;">
+          <el-table-column
+           prop="id"
+           align="center"
+           label="设备标识">
+          </el-table-column>
+          <el-table-column
+           prop="name"
+           align="center"
+           label="设备名称">
+          </el-table-column>
+          <el-table-column
+           align="center"
+           label="操作">
+           <template slot-scope="scope">
+            <el-button type="text" style="color:#F56C6C;" 
+              @click="delBingDevice(scope.row.id)">删除</el-button>
+           </template>
+          </el-table-column>
+        </el-table>
+      </el-scrollbar>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="eLDialog=false">取 消</el-button>
+        <el-button type="primary" @click="getDevices">新 增</el-button>
+      </span>
+    </el-dialog>
+    <!-- 新增访客权限 -->
+    <el-dialog :visible.sync="addLDialog" width="50%"
+      custom-class="limit-dialog"
+      title="修改访客权限" top="50vh"
+      :show-close="false"
+      :close-on-press-escape="$store.state.closeOnPresEscape"
+      :close-on-click-modal="$store.state.closeOnClickModal">
+      <el-scrollbar style="max-height:50vh">
+        <el-table :data="devices"
+          style="width:100%;"
+          @selection-change="handleCheck"
+          empty-text="暂无数据">
+          <el-table-column
+            type="selection"
+            width="55">
+          </el-table-column>
+          <el-table-column
+           prop="id"
+           align="center"
+           label="设备标识">
+          </el-table-column>
+          <el-table-column
+           prop="name"
+           align="center"
+           label="设备名称">
+          </el-table-column>
+        </el-table>
+      </el-scrollbar>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addLDialog=false">取 消</el-button>
+        <el-button type="primary" @click="subCheckDevice">提 交</el-button>
       </span>
     </el-dialog>
   </div>
@@ -144,42 +204,31 @@ export default {
   data() {
     return {
       //分页
+      //访程数据
+      threadData:[],
       pageSize:10,
       allPage:0,
       currPage:1,
-      //修改访程
+
+      //修改访程时间
       eTDialog:false,
-      devices:[],
-      threadForm:{/*访程表单*/
-        id:null,
-        startTime:null,
-        endTime:null,
-        des:'',
-        deviceIds:[]
-      },
-      threadFormRule:{
-        startTime:[
-          {required: true, message: '请选择开始时间', trigger: 'change'},
-        ],
-        endTime:[
-          {required: true, message: '请选择结束时间', trigger: 'change'},
-        ],
-        des:[
-          {validator:(rule,val,callback)=> {
-            if (val.length > 50) {
-              callback(new Error('不能超过50个字符'));
-            } else {
-              callback();
-            }
-          },trigger:'change'}
-        ],
-        deviceIds:[]
-      },
-      //访程数据
-      threadData:[],
+      endTime:null,
+      visitorId:null,
+
+      /**修改访客权限 */
+      eLDialog:false,
+      bindDevice:[],//已绑定设备
+
+      //新增权限
+      addLDialog:false,
+      devices:[],//未绑定设备数据
+      checkedDevice:[],//选中的设备
+
       //图片预览
       prePicDialog:false,
       previewImg:'',
+
+      timer:null
     }
   },
   components: {
@@ -189,69 +238,14 @@ export default {
   },
   mounted(){
     this.getThreadData();
+    this.timer = window.setInterval(() => {
+      this.getThreadData();
+    }, 30000);
+  },
+  beforeDestroy(){
+    window.clearInterval(this.timer);
   },
   methods:{
-    // 新增账户
-    handleAdd(){
-      this.addDrawer = true
-    },
-    close(formName,dialog){//关闭注册抽屉
-      /*
-        首先清空表单，然后关闭drawer
-      */
-      // this.$refs[formName].resetFields()
-      for (let key in this[formName]) {
-        this[formName][key] = null;
-      }
-      this[dialog] = false;
-    },
-    submit(){//提交访程
-    
-    },
-    deleteInfo(){//删除访程
-      this.$confirm("确定删除该条访程吗?该人员将失去访问权限。", '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        /*
-          发送请求
-          刷新人员数据
-        */
-        this.$store.commit('handleLoding');
-        request({
-          url:"/staff/delete",
-          method:"post",
-        }).then((res) =>{
-          let respond = handleRequest.call(this,res.data);
-          if (respond !== false) {
-            this.$message({
-              message: respond,
-              type: 'success'
-            });
-          }
-          //刷新数据
-        }).catch(err =>{
-          window.console.log(err)
-        }).finally(()=>{
-          this.$store.commit('handleLoding')
-        })
-      }).catch(() => {
-      
-      });
-    },
-    edit(row,formName,dialog){//编辑表格信息
-      /*
-        将表格中的信息循环赋值到表单中
-      */
-      for (const key in this[formName]) {
-        if (this[formName].hasOwnProperty(key)) {
-          this[formName][key] = row[key];
-        }
-      }
-      this[dialog] = true;
-    },
-
     //获取访程数据
     getThreadData() {
       this.$store.commit('handleLoding');
@@ -263,19 +257,21 @@ export default {
           pageSize:this.pageSize,
         }
       }).then((res) => {
-        window.console.log(res);
         let { allCount,list } = handleRequest.call(this,res.data);
         this.allPage= allCount;
         this.threadData = [];
         list.forEach(ele => {
           this.threadData.push({
+            id:ele.visitor_id,
             endTime:ele.end_time,
             startTime:ele.st_time,
             picture:ele.face_img,
             name:ele.name,
             sex:ele.sex,
             tel:ele.tel,
-            des:ele.why
+            des:ele.why,
+            surplus:ele.surplus,
+            status:ele.status
           })
         });
       }).catch((err) => {
@@ -302,53 +298,234 @@ export default {
       this.previewImg = picture;
       this.prePicDialog = true;
     },
-    //修改访程
+    /**将毫秒转化为小时分钟 */
+    formatSurplus(row, column, cellValue){
+      if (cellValue<0) {
+        return '已超时';
+      }
+      //计算小时
+      let hour = Math.floor(cellValue/3600000);
+      //计算分钟 小时的余数
+      let rem = cellValue-(hour*3600000);
+      let munites = Math.floor(rem/60000);
+      return hour+'时'+munites+'分';
+    },
+    /**时间剩余 */
+    tableRowStyle({row}){
+      if (row.surplus <= 600000 && row.surplus > 0) {
+        return {background:'rgba(230,162,60,0.3)'};
+      } else if(row.surplus <= 0){
+        return {background:'rgba(245, 108, 108, 0.3)'};
+      }
+      return '';
+    },
+
+    //修改访程时效
     editThread(id){
       this.eTDialog = true;
-      this.threadForm.id = id;
+      this.visitorId = id;
     },
-    /**提交访程信息 */
+    /**提交访程时效信息 */
     submitThread(){
-      this.$refs['threadForm'].validate((valid) => {
-        if (valid) {
-          this.$store.commit('handleLoding');
-          request({
-            url:"/visitCurrent/insert",
-            method:"post",
-            data:{
-              visitor_id:this.threadForm.id,
-              st_time:this.threadForm.startTime,
-              end_time:this.threadForm.endTime,
-              why:this.threadForm.des,
-              deviceIds:this.threadForm.deviceIds
-            }
-          }).then((res) => {
-            let respond = handleRequest.call(this,res.data);
-            if (respond !== false) {
-              this.$message({
-                message: respond,
-                type: 'success'
-              });
-            }
-            this.cancelThread();
-          }).catch((err) => {
-            window.console.log(err)
-          }).finally(()=>{
-            this.$store.commit('handleLoding');
+      if (this.endTime == null) {
+        this.$message({
+          message: '时效不能为空',
+          type: 'error'
+        });
+        return;
+      }
+      this.$store.commit('handleLoding');
+      request({
+        url:"/visitCurrent/update",
+        method:"post",
+        data:{
+          visitor_id:this.visitorId,
+          end_time:this.endTime,
+        }
+      }).then((res) => {
+        let respond = handleRequest.call(this,res.data);
+        if (respond !== false) {
+          this.$message({
+            message: respond,
+            type: 'success'
           });
         }
+        //重置
+        this.cancelThread();
+        //刷新数据
+        this.getThreadData();
+      }).catch((err) => {
+        window.console.log(err)
+      }).finally(()=>{
+        this.$store.commit('handleLoding');
       });
     },
-    /**取消提交 */
+    /**取消时效提交 */
     cancelThread(){
-      this.threadForm.id=null;
-      this.threadForm.startTime=null;
-      this.threadForm.endTime=null;
-      this.threadForm.des='';
-      this.threadForm.deviceIds=[];
-      this.$refs['threadForm'].resetFields();
+      this.endTime=null;
       this.eTDialog = false;
-    }
+    },
+
+    //修改访程权限
+    editLimit(id){
+      this.eLDialog = true;
+      this.visitorId = id;
+      this.getBingDevice();
+    },
+    getBingDevice(){//获取已绑定设备
+      this.$store.commit('handleLoding');
+      request({
+        url:"/visitCurrent/selectOnPower",
+        method:"post",
+        data:{
+          visitor_id:this.visitorId,
+        }
+      }).then((res) => {
+        this.bindDevice = [];
+        let respond = handleRequest.call(this,res.data);
+        if (respond !== false) {
+          respond.forEach(ele => {
+            this.bindDevice.push({
+              id:ele.device_id,
+              name:ele.device_name
+            });
+          });
+        }
+      }).catch((err) => {
+        window.console.log(err)
+      }).finally(()=>{
+        this.$store.commit('handleLoding');
+      });
+    },
+    delBingDevice(deviceId){//删除已绑定权限
+      this.$confirm("确定删除该条权限吗。", '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$store.commit('handleLoding');
+        request({
+          url:"/visitCurrent/delPower",
+          method:"post",
+          data:{
+            visitor_id:this.visitorId,
+            deviceIds:[deviceId]
+          }
+        }).then((res) => {
+          let respond = handleRequest.call(this,res.data);
+          if (respond !== false) {
+            this.$message({
+              message: respond,
+              type: 'success'
+            });
+          }
+          //重新获取已绑定的设备
+          this.getBingDevice();
+        }).catch((err) => {
+          window.console.log(err)
+        }).finally(()=>{
+          this.$store.commit('handleLoding');
+        });
+      }).catch(() => {
+      
+      });
+    },
+    getDevices(){//获取未绑定设备
+      this.addLDialog = true;
+      this.$store.commit('handleLoding');
+      request({
+        url:"/visitCurrent/selectUnPower",
+        method:"post",
+        data:{
+          visitor_id:this.visitorId,
+        }
+      }).then((res) => {
+        this.devices = [];
+        let respond = handleRequest.call(this,res.data);
+        if (respond !== false) {
+          respond.forEach(ele => {
+            this.devices.push({
+              id:ele.device_id,
+              name:ele.device_name
+            });
+          });
+        }
+      }).catch((err) => {
+        window.console.log(err);
+      }).finally(()=>{
+        this.$store.commit('handleLoding');
+      });
+    },
+    handleCheck(val){//选中设备监听回调
+      this.checkedDevice = [];
+      val.forEach(ele => {
+        this.checkedDevice.push(ele.id);
+      });
+    },
+    subCheckDevice(){//提交选中的设备
+      this.$store.commit('handleLoding');
+      request({
+        url:"/visitCurrent/addPower",
+        method:"post",
+        data:{
+          visitor_id:this.visitorId,
+          deviceIds:this.checkedDevice
+        }
+      }).then((res) => {
+        let respond = handleRequest.call(this,res.data);
+        if (respond !== false) {
+          this.$message({
+            message: respond,
+            type: 'success'
+          });
+        }
+        //关闭当前dialog
+        this.addLDialog = false;
+        //刷新已绑定数据
+        this.getBingDevice();
+      }).catch((err) => {
+        window.console.log(err);
+      }).finally(()=>{
+        this.$store.commit('handleLoding');
+      });
+    },
+
+    deleteInfo(id){//结束访程
+      this.$confirm("确定删除该条访程吗?该人员将失去访问权限。", '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        /*
+          发送请求
+          刷新人员数据
+        */
+        this.$store.commit('handleLoding');
+        request({
+          url:"/visitCurrent/delete",
+          method:"post",
+          data:{
+            visitor_id:id
+          }
+        }).then((res) =>{
+          let respond = handleRequest.call(this,res.data);
+          if (respond !== false) {
+            this.$message({
+              message: respond,
+              type: 'success'
+            });
+          }
+          //刷新数据
+          this.getThreadData();
+        }).catch(err =>{
+          window.console.log(err);
+        }).finally(()=>{
+          this.$store.commit('handleLoding');
+        })
+      }).catch(() => {
+      
+      });
+    },
   }
 }
 </script>
@@ -377,4 +554,6 @@ export default {
   width: 100%;
   height: auto;
 }
+
+
 </style>
